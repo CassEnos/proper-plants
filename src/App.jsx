90aafe342bc66491b6c2443cdf97ef581dfa48a4
Cass Enos <enosdoc@gmail.com>
2026-02-07 20:51:37 -0800

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import PlantList from "./components/PlantList";
+import Cart from "./components/Cart";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    const itemExists = cart.find((item) => item.id === plant.id);
+    if (itemExists) {
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        ),
+      );
+    } else {
+      setCart([...cart, { ...plant, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === itemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
+        )
+        .filter((item) => item.quantity > 0),
+    );
+  };
+
+  return (
+    <>
+      <PlantList onAddToCart={addToCart} />
+      <Cart cart={cart} onRemoveFromCart={removeFromCart} />
+    </>
+  );
 }
